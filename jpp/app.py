3a00e38cb681e7f27c4c8f0a1c67070018bf3f03
Travis Briggs <audiodude@gmail.com>
2021-03-15 19:56:29 -0700

    Connect to Mongo, retrieve the list of movies, and display it

--- jpp/app.py
+++ jpp/app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template
+from flask_session import Session
+from flask_socketio import SocketIO
+from pymongo import MongoClient
+
 app = Flask(__name__)
+Session(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+MONGO_URL = os.environ.get('MONGO_URL') or 'mongodb://localhost:27017'
+client = MongoClient(MONGO_URL,
+                     connectTimeoutMS=2000,
+                     serverSelectionTimeoutMS=6000)
+db = client.jpp
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+  movies = list(db.movies.find())
+  return render_template('index.html.jinja2', movies=movies)
